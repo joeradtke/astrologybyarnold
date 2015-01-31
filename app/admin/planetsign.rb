33d0bdd300f092ca5979c94e1cd9fa1e127a2c3b
joeradtke <joeradtke@yahoo.com>
2015-01-31 14:23:55 -0700

    finished planet signs

---
+++ app/admin/planetsign.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register Planetsign do
+  index do
+    column:planetno
+    column:signno
+    column:title
+    column:body
+    actions
+  end
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  permit_params :planetno, :signno, :title, :body
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+
+end
